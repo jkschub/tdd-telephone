b02e9f15e0afb59cc36c3e99f90508e23107a25e
Jaime Schubert <jkschub@gmail.com>
2014-04-22 10:37:42 -0500

    wrote tests to test todo list

--- todo_spec.rb
+++ todo_spec.rb
@@ -52,3 +52,28 @@ describe Task do
 		end
 	end
 end
+
+
+describe TodoList do
+	let(:todo) { todo = TodoList.new("Home Stuff") }
+
+	context "on initialize" do
+		it "should have a title attribute" do
+			expect(todo.title).to eq "Home Stuff"
+		end
+
+		it "should accept many tasks" do
+			expect(todo.list).should have(0).items
+		end
+
+	end
+
+	context "add task" do
+		it "should add task object to todo list" do
+			task = Task.new("Walk the dog", "I'm going to walk the dog.")
+			todo.add_task(task)
+			expect(todo.list).should have(1).items
+			expect(todo.list.first).should be_an_instance_of Task
+		end
+	end
+end
