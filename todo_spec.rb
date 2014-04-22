54b6757d760685b72cc794ce83d1c3bcb7724d18
Jaime Schubert <jkschub@gmail.com>
2014-04-22 09:31:43 -0500

    test for attributes there

--- todo_spec.rb
+++ todo_spec.rb
@@ -1,19 +1,23 @@
 require "rspec"
 
-describe Todo do
-	# let(:task) { task = Todo.new() }
+describe Task do
+	let(:task) { task = Task.new("Walk the dog", "I'm walking the dog", "incomplete", Time.now) }
 
 	context "when initializing" do
 		it "should have a title" do
+			expect(task.title).to eq "Walk the dog"
 		end
 
 		it "should have a description" do
+			expect(task.description).to eq "I'm walking the dog"
 		end
 
 		it "should have a status" do
+			expect(task.status).to eq "incomplete"
 		end
 
 		it "should have a created_at timestamp" do
+			expect(task.created_at).should be_a_kind_of(Time)
 		end
 	end
 end
