b099159e7e70c45829fc89eec15e4eea04fb7329
Jaime Schubert <jkschub@gmail.com>
2014-04-22 09:59:16 -0500

    tests for task completion methods

--- todo_spec.rb
+++ todo_spec.rb
@@ -31,4 +31,34 @@ describe Task do
 			expect(task.created_at).should be_a_kind_of(Time)
 		end
 	end
+
+	context "method to mark as complete" do
+		it "should set status to complete" do
+			task.mark_as_complete!
+
+			expect(task.status).to eq "complete"
+		end
+	end
+
+	context "method to mark as incomplete" do
+		it "should set status to incomplete" do
+			task.mark_as_incomplete!
+
+			expect(task.status).to eq "incomplete"
+		end
+	end
+
+	context "method to check if complete" do
+		it "should show the status is complete" do
+			task.mark_as_complete!
+
+			expect(task.complete?).to eq true
+		end
+
+		it "should show the status is incomplete" do
+			task.mark_as_incomplete!
+
+			expect(task.complete?).to eq false
+		end
+	end
 end
