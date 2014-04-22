c1a4a293014e2bc1cfb6effc63eafc5321a6dfc7
Jaime Schubert <jkschub@gmail.com>
2014-04-22 09:25:23 -0500

    new todo spec file

---
+++ todo_spec.rb
@@ -0,0 +1,19 @@
+require "rspec"
+
+describe Todo do
+	# let(:task) { task = Todo.new() }
+
+	context "when initializing" do
+		it "should have a title" do
+		end
+
+		it "should have a description" do
+		end
+
+		it "should have a status" do
+		end
+
+		it "should have a created_at timestamp" do
+		end
+	end
+end
